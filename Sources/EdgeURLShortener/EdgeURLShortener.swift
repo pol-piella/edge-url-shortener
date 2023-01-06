789aa55aa6d29a1966a853febae1fa5a7cf3c523
Pol Piella Abadia <hi@polpiella.dev>
2023-01-06 10:43:40 +0000

    Creates shell executable swift package

---
+++ Sources/EdgeURLShortener/EdgeURLShortener.swift
@@ -0,0 +1,8 @@
+@main
+public struct EdgeURLShortener {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(EdgeURLShortener().text)
+    }
+}
---
+++ Tests/EdgeURLShortenerTests/EdgeURLShortenerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import EdgeURLShortener
+
+final class EdgeURLShortenerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(EdgeURLShortener().text, "Hello, World!")
+    }
+}
