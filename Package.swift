cf486966bc10ce03580a1dac23b9d6325bd7b8d1
Pol Piella Abadia <hi@polpiella.dev>
2023-01-06 10:54:12 +0000

    Imports `Redis` client

--- Package.swift
+++ Package.swift
@@ -9,13 +9,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/swift-cloud/Compute", exact: "2.8.0")
+        .package(url: "https://github.com/swift-cloud/Compute", exact: "2.8.0"),
+        .package(url: "https://github.com/swift-cloud/Upstash.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "EdgeURLShortener",
-            dependencies: ["Compute"])
+            dependencies: ["Compute", "Upstash"])
     ]
 )
