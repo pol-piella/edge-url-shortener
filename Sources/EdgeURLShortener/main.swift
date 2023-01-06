ecf3ec2021036696372794d3f5240db11b94b822
Pol Piella Abadia <hi@polpiella.dev>
2023-01-06 11:24:03 +0000

    Revert "Attempts to fix internal server error"
    This reverts commit d683d4225ebb43803a3f2064a626fd2d58deae27.

--- Sources/EdgeURLShortener/main.swift
+++ Sources/EdgeURLShortener/main.swift
@@ -3,5 +3,7 @@ import Upstash
 
 try await onIncomingRequest { req, res in
     let secrets = try ConfigStore(name: "secrets")
-    try await res.status(200).send(secrets.get("UPSTASH_HOST_NAME") ?? "Hello!")
+    let redisClient = RedisClient(hostname: secrets.get("UPSTASH_HOST_NAME") ?? "", token: secrets.get("UPSTASH_TOKEN") ?? "")
+    let newsletter: String = try await redisClient.get("newsletter")
+    try await res.status(200).send(newsletter)
 }
