f4cf7aaf7a35a097e3d46f375e4b001c95bd15d7
Pol Piella Abadia <hi@polpiella.dev>
2023-01-06 11:25:17 +0000

    Performs permanent redirect as a response

--- Sources/EdgeURLShortener/main.swift
+++ Sources/EdgeURLShortener/main.swift
@@ -5,5 +5,5 @@ try await onIncomingRequest { req, res in
     let secrets = try ConfigStore(name: "secrets")
     let redisClient = RedisClient(hostname: secrets.get("UPSTASH_HOST_NAME") ?? "", token: secrets.get("UPSTASH_TOKEN") ?? "")
     let newsletter: String = try await redisClient.get("newsletter")
-    try await res.status(200).send(newsletter)
+    try await res.redirect(newsletter, permanent: true)
 }
